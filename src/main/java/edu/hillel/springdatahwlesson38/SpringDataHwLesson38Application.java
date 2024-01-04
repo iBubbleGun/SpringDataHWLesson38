56a864b781e25423c66e44c4234c36eba46a37aa
Tymur Kosiak <us1gav@gmail.com>
2024-01-05 00:17:00 +0200

    Spring Data homework lesson38 create.

---
+++ src/main/java/edu/hillel/springdatahwlesson38/SpringDataHwLesson38Application.java
@@ -0,0 +1,13 @@
+package edu.hillel.springdatahwlesson38;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataHwLesson38Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataHwLesson38Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hillel/springdatahwlesson38/SpringDataHwLesson38ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hillel.springdatahwlesson38;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataHwLesson38ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
